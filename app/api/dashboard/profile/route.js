1e9f83abf220cb9ca4b0ed8cc83bac4f5bebcaa2
Biplob <mdbiplub13@gmail.com>
2025-11-24 17:17:45 +0600

    profile work done without verifycation

--- app/api/dashboard/profile/route.js
+++ app/api/dashboard/profile/route.js
@@ -1,6 +1,7 @@
 import User from "@/app/models/userModel";
 import connectMongoDb from "@/lib/mongoose";
 import { NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
 
 // Utility: keep only allowed fields
 const pickFields = (data, allowed) => {
@@ -76,5 +77,57 @@ export async function PUT(req) {
       },
       { status: 500 }
     );
+  }
+}
+
+export async function PATCH(req) {
+  try {
+    await connectMongoDb();
+
+    const {userId, oldPassword, newPassword} = await req.json();
+    if (!userId || !oldPassword || !newPassword) {
+      return NextResponse.json(
+        { status: "fail", message: "Invalid request" },
+        { status: 400 }
+      );
+    }
+
+    const user = await User.findById(userId);
+    if (!user) {
+      return NextResponse.json(
+        { status: "fail", message: "User not found" },
+        { status: 404 }
+      );
+    }
+
+    // Check if old password is correct
+    const isMatch = await bcrypt.compare(oldPassword, user.password);
+    if (!isMatch) {
+      return NextResponse.json(
+        { status: "fail", message: "Old password is incorrect" },
+        { status: 400 }
+      );
+    }
+
+    // Hash the new password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(newPassword, salt);
+
+    // Update the password
+    user.password = hashedPassword;
+    await user.save();
+
+    return NextResponse.json({
+      status: "success",
+      message: "Password updated successfully",
+    });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        status: "fail",
+        message: error.message,
+      },
+      { status: 500 }
+    )
   }
 }
